70f8dd5a5b2cec2cda79d3d1170697cec8384bdf
gpdl01 <119022228+gpdl01@users.noreply.github.com>
2024-03-01 19:35:39 -0300

    Update mult_target_ping.ps1

--- mult_target_ping.ps1
+++ mult_target_ping.ps1
@@ -1,24 +1,31 @@
-$Output= @()
-$names = Get-content "target-IPs.txt"
-$pcname = $env:COMPUTERNAME #get computer name to save where ping come from
-$host.ui.RawUI.WindowTitle = “NET-Watcher#by gpdl” # custom window name
-
+$downcounter = 0
+$maxdowncount = 5
+$sendtotelegram = 1
 while ($true) {
-    foreach ($name in $names){ #looping on target-IPs list
-      $Time = (Get-Date).ToString(“HH:mm:ss”) #get time
-      $Date = Get-Date -Format dd.MM.yyyy #get date
-      $response = (Test-Connection -ComputerName $name -Count 2 -BufferSize 30 -ErrorAction SilentlyContinue | measure-Object -Property ResponseTime -Average).average #ping command config and average
+    foreach ($name in $names){
+      $Time = (Get-Date).ToString(“HH:mm:ss”)
+      $Date = Get-Date -Format dd.MM.yyyy
+      $response = (Test-Connection -ComputerName $name -Count 2 -BufferSize 54 -ErrorAction SilentlyContinue | measure-Object -Property ResponseTime -Average).average
       if ($response -ne $null){
-        $response = ($response -as [int] ) #rouding response value
-        $Output+= "$Date,$Time,$name,up,$response ms,$pcname" #writing variables to file
-        Write-Host "$Date,$Time,$Name,up,$response ms" -ForegroundColor Green #printing variables
+        $response = ($response -as [int] ) 
+        $Output+= "$Date,$Time,$name,up,$response ms"
+        Write-Host "$Date,$Time,$Name,up,$response ms" -ForegroundColor Green
       }else{
         $Output+= "$Date,$Time,$name,down,no response"
         Write-Host "$Date,$Time,$Name,down,no response" -ForegroundColor Red
+	    if ($sendtotelegram -eq 1){
         #Invoke-RestMethod -Uri "https://api.telegram.org/bot%YOUR TOKEN HEREsendMessage?chat_id=-%YOUR ID HERE%&text=$pcname->$Name-caiu-" -Method Post #sending msgs to your Telegram Bot
+        }
+        $downcounter = $downcounter + 1
+
+        if ($downcounter -ge $maxdowncount) {
+        Write-Host "$Name will not come back! Telegram alerts disabled." -ForegroundColor YELLOW
+        Start-Sleep -Seconds 1
+	    $downcounter = 0
+        $sendtotelegram = 0
+        }
       }
     }
-    $Output | Out-file ping-logs.csv #saving to file
-    Start-Sleep -Seconds 60 #delay time between loops
-    # Start-Sleep -Millis 300
+    $Output | Out-file result.csv
+    Start-Sleep -Seconds 5
 }
